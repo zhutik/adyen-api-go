71b7b3179877435b4ffb5227d8f2a30f74500685
Igor Zhutaiev <igor.zhutaiev@westwing.de>
2017-07-02 17:17:23 +0200

    Another try with multiline godoc block

--- credetials.go
+++ credetials.go
@@ -9,6 +9,8 @@ package adyen
 //     Password - API password for authentication
 //     merchantID - Merchant Account settings for payment and modification requests
 //     clientID - Used to load external JS files from Adyen, to encrypt client requests
+//
+// You can create new API user there: https://ca-test.adyen.com/ca/ca/config/users.shtml
 type apiCredentials struct {
 	env         Environment
 	Username    string
@@ -25,6 +27,8 @@ type apiCredentials struct {
 //     hmac - is generated when new Skin is created in Adyen Customer Area
 //     skinCode - skin code from Adyen CA
 //     shopperLocale - merchant local settings (in ISO format)
+//
+// New skin can be created there https://ca-test.adyen.com/ca/ca/skin/skins.shtml
 type hppCredentials struct {
 	hmac          string
 	skinCode      string
