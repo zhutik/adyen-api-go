55909b71b130103a4b45f3c825b8d0273c166e7c
LBailly <ludovic.bailly@gmail.com>
2017-07-26 08:48:05 +0200

    Add recurring payment support Add new fields to support 3DS call Add authorise3d Adyen endpoint

--- payment.go
+++ payment.go
@@ -1,19 +1,34 @@
 package adyen
 
+const (
+	RECURRING = "RECURRING"
+	ONECLICK  = "ONECLICK"
+)
+
 // AuthoriseEncrypted structure for Authorisation request (with encrypted card information)
 type AuthoriseEncrypted struct {
 	AdditionalData  *AdditionalData `json:"additionalData"`
 	Amount          *Amount         `json:"amount"`
 	Reference       string          `json:"reference"`
 	MerchantAccount string          `json:"merchantAccount"`
+	// Mandatory for recurring payment
+	ShopperReference string `json:"shopperReference,omitempty"`
+	Recurring       *Recurring      `json:"recurring,omitempty"`
+	// Required for a 3DS process
+	BrowserInfo *BrowserInfo `json:"browserInfo,omitempty"`
 }
 
 // Authorise structure for Authorisation request (card is not encrypted)
 type Authorise struct {
-	Card            *Card   `json:"card"`
-	Amount          *Amount `json:"amount"`
-	Reference       string  `json:"reference"`
-	MerchantAccount string  `json:"merchantAccount"`
+	Card            *Card      `json:"card"`
+	Amount          *Amount    `json:"amount"`
+	Reference       string     `json:"reference"`
+	MerchantAccount string     `json:"merchantAccount"`
+	// Mandatory for recurring payment
+	ShopperReference string `json:"shopperReference,omitempty"`
+	Recurring       *Recurring `json:"recurring,omitempty"`
+	// Required for a 3DS process
+	BrowserInfo *BrowserInfo `json:"browserInfo,omitempty"`
 }
 
 // AuthoriseResponse is a response structure for Adyen
@@ -22,9 +37,22 @@ type AuthoriseResponse struct {
 	ResultCode    string `json:"resultCode"`
 	AuthCode      string `json:"authCode"`
 	RefusalReason string `json:"refusalReason"`
+	IssuerUrl string `json:"issuerUrl"`
+	MD string `json:"md"`
+	PaRequest string `json:"paRequest"`
 }
 
 // AdditionalData stores encrypted information about customer's credit card
 type AdditionalData struct {
 	Content string `json:"card.encrypted.json"`
+}
+
+// BrowserInfo hold information on the user browser
+type BrowserInfo struct {
+	AcceptHeader string `json:"acceptHeader"`
+	UserAgent    string `json:"userAgent"`
+}
+
+type Recurring struct {
+	Contract string `json:"contract"`
 }
---
+++ payment3d.go
@@ -0,0 +1,10 @@
+package adyen
+
+// Authorise structure for Authorisation request (card is not encrypted)
+type Authorise3D struct {
+	MD string `json:"md"`
+	MerchantAccount string     `json:"merchantAccount"`
+	BrowserInfo *BrowserInfo `json:"browserInfo"`
+	PaResponse string `json:"paResponse"`
+	ShopperIP string `json:"shopperIP,omitempty"`
+}
