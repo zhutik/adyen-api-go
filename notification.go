bab6487d3d338da989834234a70e77109a0d5f86
codingconcepts <bassrob@gmail.com>
2018-02-07 13:52:17 +0000

    Added named struct for NotificationRequestItem data, to make testing easier for consumers.

--- notification.go
+++ notification.go
@@ -16,26 +16,29 @@ type NotificationRequest struct {
 //
 // Link - https://docs.adyen.com/developers/api-reference/notifications-api#notificationrequestitem
 type NotificationRequestItem struct {
-	NotificationRequestItem struct {
-		AdditionalData struct {
-			ShopperReference         string `json:"shopperReference,omitempty"`
-			ShopperEmail             string `json:"shopperEmail,omitempty"`
-			AuthCode                 string `json:"authCode,omitempty"`
-			CardSummary              string `json:"cardSummary,omitempty"`
-			ExpiryDate               string `json:"expiryDate,omitempty"`
-			AuthorisedAmountValue    string `json:"authorisedAmountValue,omitempty"`
-			AuthorisedAmountCurrency string `json:"authorisedAmountCurrency,omitempty"`
-		} `json:"additionalData,omitempty"`
-		Amount              Amount     `json:"amount"`
-		PspReference        string     `json:"pspReference"`
-		EventCode           string     `json:"eventCode"`
-		EventDate           time.Time  `json:"eventDate"` // Event date in time.RFC3339 format
-		MerchantAccountCode string     `json:"merchantAccountCode"`
-		Operations          []string   `json:"operations"`
-		MerchantReference   string     `json:"merchantReference"`
-		OriginalReference   string     `json:"originalReference,omitempty"`
-		PaymentMethod       string     `json:"paymentMethod"`
-		Reason              string     `json:"reason,omitempty"`
-		Success             stringBool `json:"success"`
-	} `json:"NotificationRequestItem"`
+	NotificationRequestItem NotificationRequestItemData `json:"NotificationRequestItem"`
+}
+
+// NotificationRequestItemData contains the NotificationRequestItem data.
+type NotificationRequestItemData struct {
+	AdditionalData struct {
+		ShopperReference         string `json:"shopperReference,omitempty"`
+		ShopperEmail             string `json:"shopperEmail,omitempty"`
+		AuthCode                 string `json:"authCode,omitempty"`
+		CardSummary              string `json:"cardSummary,omitempty"`
+		ExpiryDate               string `json:"expiryDate,omitempty"`
+		AuthorisedAmountValue    string `json:"authorisedAmountValue,omitempty"`
+		AuthorisedAmountCurrency string `json:"authorisedAmountCurrency,omitempty"`
+	} `json:"additionalData,omitempty"`
+	Amount              Amount     `json:"amount"`
+	PspReference        string     `json:"pspReference"`
+	EventCode           string     `json:"eventCode"`
+	EventDate           time.Time  `json:"eventDate"` // Event date in time.RFC3339 format
+	MerchantAccountCode string     `json:"merchantAccountCode"`
+	Operations          []string   `json:"operations"`
+	MerchantReference   string     `json:"merchantReference"`
+	OriginalReference   string     `json:"originalReference,omitempty"`
+	PaymentMethod       string     `json:"paymentMethod"`
+	Reason              string     `json:"reason,omitempty"`
+	Success             stringBool `json:"success"`
 }
