9851b7f8a15ef6ebed84395c858730ed3485917f
Ajuna Kaliantiga <ajuna.kaliantiga@happyfresh.com>
2019-05-16 12:45:25 +0700

    add funding source

--- payment.go
+++ payment.go
@@ -36,6 +36,7 @@ type AuthoriseEncrypted struct {
 	SelectedRecurringDetailReference string          `json:"selectedRecurringDetailReference,omitempty"`
 	BrowserInfo                      *BrowserInfo    `json:"browserInfo,omitempty"` // Required for a 3DS process
 	CaptureDelayHours                *int            `json:"captureDelayHours,omitempty"`
+	FundingSource                    string          `json:"fundingSource,omitempty"`
 }
 
 // Authorise structure for Authorisation request (card is not encrypted)
